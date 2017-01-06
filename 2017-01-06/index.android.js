9c4ee2436505b8434265b2ae1c2177515e4eb0b4
Kamil Szot <kamil.szot@gmail.com>
2017-01-06 10:31:19 +0100

    React native hello world with async support

---
+++ 2017-01-06/index.android.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Button,
+  View,
+  DatePickerAndroid
+} from 'react-native';
+
+
+export default class AwesomeProject extends Component {
+  async pickDate() {
+    const {action, year, month, day} = await DatePickerAndroid.open({
+        // Use `new Date()` for current date.
+        // May 25 2020. Month 0 is January.
+        date: new Date(2020, 4, 25)
+    });
+    if (action !== DatePickerAndroid.dismissedAction) {
+      // Selected year, month (0-11), day
+    }
+    
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+        <Button style={styles.button} onPress={this.pickDate} title="Pick date" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  button: {
+    top: 15
+  }
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
