a481be25b4a19a750995abcc11b4183efbd31523
Kamil Szot <kamil.szot@gmail.com>
2017-02-01 23:03:34 +0100

    Clojure hello world as edited in VS Core

---
+++ 2017-02-01/README.md
@@ -0,0 +1,9 @@
+Commands
+========
+
+    lein self-install
+    lein new my-project-name
+    lein repl
+
+
+    Ctrl+P Eval in VS Code.
---
+++ 2017-02-01/hello-vscoder/src/hello_vscoder/core.clj
@@ -0,0 +1,17 @@
+(ns hello-vscoder.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!!!"))
+
+(foo "Johnny")
+
+
+
+
+
+
+
+
+
