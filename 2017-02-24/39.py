410c875c5558f857384876ff0548dd2d1650976c
Kamil Szot <kamil.szot@gmail.com>
2017-02-25 10:15:51 +0100

    Euler problem 39 in python, with non-stupid way of ensuring uniqueness of triangles.

--- 2017-02-24/39.py
+++ 2017-02-24/39.py
@@ -7,17 +7,16 @@ uni = set()
 for p in range(1000, 6, -1):
     for a in range(1, p-1):
         min_b = (p-2*a) // 2 + 1
-        for b in range(min_b, p-a):
+        for b in range(max(a+1, min_b), p-a):
             c = p - a - b
-            tri = tuple(sorted([a,b,c]))
-            if a+b>c and a+c>b and b+c>a and (a*a + b*b) == c*c and not tri in uni:
-                uni.add(tri)
+            if c <= b:
+                break
+            if (a*a + b*b) == c*c:
                 counts[p]+=1
 
 max_p = max(counts, key=counts.get)
 print(max_p)
-print(sum([counts[k] for k in counts]))
-print(counts)
+
 
 
                 
