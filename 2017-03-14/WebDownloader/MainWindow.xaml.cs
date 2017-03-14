6b41f4451ea1265720a2a9fcecfcf7768be5ffdd
Kamil Szot <kamil.szot@gmail.com>
2017-03-15 00:40:53 +0100

    WPF desktop program that loads data from web with async button click

---
+++ 2017-03-14/WebDownloader/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Net;
+
+namespace FacebookLoginApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+
+
+    public partial class MainWindow : Window
+    {
+        private const string ExtendedPermissions = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            WebClient client = new WebClient();
+            String downloadedString = await client.DownloadStringTaskAsync(new Uri("https://cdnjs.cloudflare.com/ajax/libs/riot/3.3.2/riot.min.js"));
+            resultTextBox.Text = downloadedString;
+
+        }
+
+   
+    }
+}
