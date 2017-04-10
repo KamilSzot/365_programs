2827e2a38a1fedd3080c1919a6f950d89dd71a63
Kamil Szot <kamil.szot@gmail.com>
2017-04-10 02:06:02 +0200

    Simple UDP client in C#

---
+++ 2017-04-09/UdpClient/UdpClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UdpClient client = new UdpClient(22000))
+            {
+                var stop = false;
+                Task.Run(async () =>
+                {
+                    while (!stop)
+                    {
+                        var packet = await client.ReceiveAsync();
+                        var payload = Encoding.ASCII.GetString(packet.Buffer);
+                        Console.WriteLine(payload);
+                        if (payload == "stop")
+                        {
+                            stop = true;
+                        }
+                    }
+                }).Wait();
+            }
+            
+        }
+    }
+}
